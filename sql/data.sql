862180cb4987243822bbcb6dcabb9affcfe44acf
khiem <khiem@umich.edu>
2024-04-13 16:15:53 -0400

    initial directory structure

---
+++ sql/data.sql
@@ -0,0 +1,7 @@
+PRAGMA foreign_keys = ON;
+
+INSERT INTO pets (petname, info, petfilename)
+VALUES ('test', 'this pet is a test. i hope i can see this message!', 'pet00.jpg');
+
+INSERT INTO pets (petname, info, petfilename)
+VALUES ('shmeagle', 'this is shmeagle, the cutest kitten!', 'pet00.jpg');

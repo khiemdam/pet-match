6b7f2fda7d7908cd90a10da5bdefd9ba745e8459
khiem <khiem@umich.edu>
2024-04-15 08:19:51 -0400

    Completed the left swipe/no button implementation

--- js/pet_match.js
+++ js/pet_match.js
@@ -13,10 +13,28 @@ $(document).ready(function() {
     });
     // make the pet card moveable
     var pets = [];
+    var curr_pet_index = 0;
     load_pets(pets);
     shuffle_pets(pets);
     load_first_pet(pets);
-    var curr_pet_index = 1;
+    // handle swipes
+    $('.pet-card').on("swipeleft", function() {
+        ++curr_pet_index;
+        left_swipe(pets, curr_pet_index);
+    });
+    $('.pet-card').on("swiperight", function() {
+        ++curr_pet_index;
+        left_swipe(pets, curr_pet_index);
+    });
+    // handle button clicks
+    $('.no-button').on("click", function() {
+        ++curr_pet_index;
+        left_swipe(pets, curr_pet_index);
+    });
+    $('.yes-button').on("click", function() {
+        ++curr_pet_index;
+        left_swipe(pets, curr_pet_index);
+    });
 })
 
 function load_pets(pets_array) {
@@ -43,13 +61,55 @@ function shuffle_pets(pets_array) {
 function load_first_pet(pets_array) {
     var pet = pets_array[0];
     var pet_html = `
-            <div class="pet-card-item" id="hammer">
-                <img src="static/images/${pet.file}" alt="${pet.name}">
-                <div class="pet-info">
-                    <h2>${pet.name}, ${pet.age}</h2>
-                    <p>${pet.info}</p>
-                </div>
+        <img src="static/images/${pet.file}" alt="${pet.name}">
+        <div class="pet-info">
+            <h2>${pet.name}, ${pet.age}</h2>
+            <p>${pet.info}</p>
+        </div>`;
+    $('.pet-card').html(pet_html);
+}
 
-            </div>`;
-    $('.pet_card').append(pet_html);
+function left_swipe(pets, curr_pet_index) {
+    $('.pet-card').addClass("slide-left");
+    $('.pet-card').on('transitionend', function() {
+        $('.pet-card').addClass("teleport-in");
+        $('.pet-card').on('transitionend', function() {
+            $('.pet-card').removeClass('slide-left');
+            if (curr_pet_index < pets.length) {
+                load_next_pet(pets, curr_pet_index);
+            }
+            else {
+                console.log(curr_pet_index);
+                end_card();
+            }
+            $('.pet-card').removeClass('teleport-in');
+            $('.pet-card').addClass('fade-in');
+            $('.pet-card').on('transitionend', function() {
+                $('.pet-card').removeClass('fade-in');
+            })
+        })
+    });
+}
+
+function load_next_pet(pets_array, curr_pet_index) {
+    console.log("test");
+    console.log(curr_pet_index);
+    var pet = pets_array[curr_pet_index];
+    var pet_html = `
+        <img src="static/images/${pet.file}" alt="${pet.name}">
+        <div class="pet-info">
+            <h2>${pet.name}, ${pet.age}</h2>
+            <p>${pet.info}</p>
+            <p>${curr_pet_index}</p>
+        </div>`;
+    $('.pet-card').html(pet_html);
+}
+
+function end_card() {
+    var pet_html = `
+        <div class="end-info">
+            <h2>No More pets!</h2>
+            <p>Come back later to find new pets waiting to be adopted :)</p>
+        </div>`;
+    $('.pet-card').html(pet_html);
 }

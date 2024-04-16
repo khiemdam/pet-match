154a15fa84b93ddf07a0ea6af216fe66504bb530
khiem <khiem@umich.edu>
2024-04-16 02:36:33 -0400

    implemented right swipe functionality

--- js/pet_match.js
+++ js/pet_match.js
@@ -1,19 +1,38 @@
 $(document).ready(function() {
     console.log("successfully linked js");
+    // initialize css
+    $('.dropdown-content a').css("color", "#FF8FAB");
+    $('.buttons2 a').css("color", "#FF8FAB");
+
+    // render pages
+    $('.home, .logo').on("click", render_home); // by default, we will render the home page
+    $('.discover').on("click", render_discover);
+    $('.contact').on("click", render_contact);
+    $('.about').on("click", render_about);
+
     // dropdown menu
     $('.dropdown-button').on('click', function() {
         // JQuery handles animations for me!
         $('.dropdown-content').slideToggle("slow");
-        $('.dropdown-content a').css("color", "#FF8FAB");
         console.log("clicked on dropdown button");
     });
+
     // animate buttons
-    $('.dropdown-button, .yes-button, .no-button').hover(function() {
-        $(this).animate({width: '+=15px'});
+    $('.logo img, .dropdown-button, .yes-button, .no-button').hover(function() {
+        $(this).animate({width: '+=20px'});
     }, function() {
-        $(this).animate({width: '-=15px'});
+        $(this).animate({width: '-=20px'});
     });
-    // make the pet card moveable
+    $('.adopt, .continue, .dropdown-content a').hover(function() {
+        $(this).animate({fontSize: '+=10px', fontWeight: 'bold'}, 200);
+        $(this).css("color", "black");
+        $(this).css("background-color", "pink");
+    }, function() {
+        $(this).animate({fontSize: '-=10px', fontWeight: 'normal'}, 200);
+        $(this).css("color", "#FF8FAB");
+        $(this).css("background-color", "transparent");
+    });
+
     var pets = [];
     var curr_pet_index = 0;
     load_pets(pets);
@@ -37,13 +56,42 @@ $(document).ready(function() {
         ++curr_pet_index;
         right_swipe(pets, curr_pet_index);
     });
+    $('.continue').on("click", function() {
+        $('.pet-yes').css("display", "none");
+        continue_right_swipe(pets, curr_pet_index);
+    });
 })
 
+function render_home() {
+
+}
+
+function render_discover() {
+
+}
+
+function render_about() {
+
+}
+
+function render_contact() {
+
+}
+
 function load_pets(pets_array) {
-    pets_array.push(new Object({ name: "Shmeagle", info: "The cutest kitten!", age: "5 months", file: "pet00.jpeg" }));
-    pets_array.push(new Object({ name: "Mittens", info: "The cutest mitten!", age: "5 months",  file: "pet00.jpeg" }));
-    pets_array.push(new Object({ name: "Destroyer of Worlds", info: "Parrot", age: "5 months",  file: "pet00.jpeg" }));
-    pets_array.push(new Object({ name: "Poochie", info: "The cutest doggie!", age: "5 months",  file: "pet00.jpeg" }));
+    pets_array.push(new Object({ name: "Bob Barker", info: "Border Collie & Siberian Husky Mix", age: "Adult Male Medium", file: "bobbarker.jpeg", url: "https://www.petfinder.com/dog/bob-barker-71227564/mi/ann-arbor/humane-society-of-huron-valley-mi175/"}));
+    pets_array.push(new Object({ name: "Fozzie", info: "Domestic Short Hair", age: "Senior Male Medium",  file: "fozzie.jpeg", url: "https://www.petfinder.com/cat/fozzie-71330227/mi/ann-arbor/humane-society-of-huron-valley-mi175/"}));
+    pets_array.push(new Object({ name: "Hypatia", info: "Domestic Short Hair", age: "Adult Female Medium",  file: "hypatia.png", url: "https://www.petfinder.com/cat/hypatia-71263576/mi/ann-arbor/humane-society-of-huron-valley-mi175/"}));
+    pets_array.push(new Object({ name: "Hoss", info: "Pit Bull Terrier", age: "Adult Male Large",  file: "hoss.png", url: "https://www.petfinder.com/dog/hoss-71004801/mi/ann-arbor/humane-society-of-huron-valley-mi175/"}));
+    pets_array.push(new Object({ name: "Tito", info: "Pit Bull Terrier", age: "Adult Male Medium",  file: "tito.png", url: "https://www.petfinder.com/dog/tito-71039560/mi/ann-arbor/humane-society-of-huron-valley-mi175/"}));
+    pets_array.push(new Object({ name: "Fluffy", info: "Domestic Short Hair", age: "Adult Female Medium",  file: "fluffy.png", url: "https://www.petfinder.com/cat/fluffy-71303992/mi/ann-arbor/humane-society-of-huron-valley-mi175/"}));
+    pets_array.push(new Object({ name: "Winger", info: "Domestic Medium Hair", age: "Young Male Medium",  file: "winger.png", url: "https://www.petfinder.com/cat/winger-49961156/mi/manchester/happy-hearts-feline-rescue-mi951/"}));
+    pets_array.push(new Object({ name: "Jojo", info: "Domestic Short Hair", age: "Adult Male Medium",  file: "jojo.png", url: "https://www.petfinder.com/cat/jo-jo-71214479/mi/ann-arbor/humane-society-of-huron-valley-mi175/"}));
+    pets_array.push(new Object({ name: "BB", info: "Rabbit Rex", age: "Adult Female Small",  file: "bb.png", url: "https://www.petfinder.com/rabbit/bb-70938351/mi/ann-arbor/humane-society-of-huron-valley-mi175/"}));
+    pets_array.push(new Object({ name: "Maisy", info: "Rabbit Bunny Rabbit", age: "Adult Female Small",  file: "maisy.png", url: "https://www.petfinder.com/rabbit/maisy-48066128/mi/whittaker/great-lakes-rabbit-sanctuary-mi120/"}));
+    pets_array.push(new Object({ name: "Sunny (Sundance)", info: "Domestic Short Hair", age: "Adult Male Large",  file: "sunny.png", url: "https://www.petfinder.com/cat/sunny-sundance-65783931/mi/manchester/happy-hearts-feline-rescue-mi951/"}));
+    pets_array.push(new Object({ name: "Jolie", info: "Pit Bull Terrier Mix", age: "Adult Female Medium",  file: "jolie.png", url: "https://www.petfinder.com/dog/jolie-71304002/mi/ann-arbor/humane-society-of-huron-valley-mi175/"}));
+    pets_array.push(new Object({ name: "Cinderella", info: "Calico", age: "Young Female Medium",  file: "cinderella.png", url: "https://www.petfinder.com/cat/cinderella-54337933/mi/manchester/happy-hearts-feline-rescue-mi951/"}));
 }
 
 function shuffle_pets(pets_array) {
@@ -69,28 +117,32 @@ function load_first_pet(pets_array) {
             <p>${pet.info}</p>
         </div>`;
     $('.pet-card').html(pet_html);
+    var yes_html = `
+        <h2>You Matched With ${pet.name}!<h2>
+        <p>Do you want to invite ${pet.name} into your family?</p>
+    `;
+    $('.yes-text').html(yes_html);
+    $('.adopt').attr('href', pet.url);
+}
+
+function display_adoption_options() {
+    $('.pet-yes').fadeIn("slow");
 }
 
 function left_swipe(pets, curr_pet_index) {
     if (curr_pet_index <= pets.length) {
         $('.pet-card').addClass("slide-left");
         $('.pet-card').on('transitionend', function() {
-            $('.pet-card').addClass("teleport-in");
-            $('.pet-card').on('transitionend', function() {
-                $('.pet-card').removeClass('slide-left');
-                if (curr_pet_index < pets.length) {
-                    load_next_pet(pets, curr_pet_index);
-                }
-                else {
-                    console.log(curr_pet_index);
-                    end_card();
-                }
-                $('.pet-card').removeClass('teleport-in');
-                $('.pet-card').addClass('fade-in');
-                $('.pet-card').on('transitionend', function() {
-                    $('.pet-card').removeClass('fade-in');
-                })
-            })
+            $('.pet-card').css("display", "none");
+            $('.pet-card').removeClass('slide-left');
+            if (curr_pet_index < pets.length) {
+                load_next_pet(pets, curr_pet_index);
+            }
+            else {
+                console.log(curr_pet_index);
+                end_card();
+            }
+            $('.pet-card').fadeIn('fast');
         });
     }
 }
@@ -99,24 +151,22 @@ function right_swipe(pets, curr_pet_index) {
     if (curr_pet_index <= pets.length) {
         $('.pet-card').addClass("slide-right");
         $('.pet-card').on('transitionend', function() {
-            $('.pet-card').addClass("teleport-in");
-            $('.pet-card').on('transitionend', function() {
-                $('.pet-card').removeClass('slide-right');
-                if (curr_pet_index < pets.length) {
-                    load_next_pet(pets, curr_pet_index);
-                }
-                else {
-                    console.log(curr_pet_index);
-                    end_card();
-                }
-                $('.pet-card').removeClass('teleport-in');
-                $('.pet-card').addClass('fade-in');
-                $('.pet-card').on('transitionend', function() {
-                    $('.pet-card').removeClass('fade-in');
-                })
-            })
+            $('.pet-card').css("display", "none");
+            $('.pet-card').removeClass('slide-right');
         });
+        display_adoption_options();
     }
+}
+
+function continue_right_swipe(pets, curr_pet_index) {
+    if (curr_pet_index < pets.length) {
+        load_next_pet(pets, curr_pet_index);
+    }
+    else {
+        console.log(curr_pet_index);
+        end_card();
+    }
+    $('.pet-card').fadeIn("slow");
 }
 
 function load_next_pet(pets_array, curr_pet_index) {
@@ -130,12 +180,18 @@ function load_next_pet(pets_array, curr_pet_index) {
             <p>${pet.info}</p>
         </div>`;
     $('.pet-card').html(pet_html);
+    var yes_html = `
+        <h2>You Matched With ${pet.name}!<h2>
+        <p>Do you want to invite ${pet.name} into your family?</p>
+    `;
+    $('.yes-text').html(yes_html);
+    $('.adopt').attr('href', pet.url);
 }
 
 function end_card() {
     var pet_html = `
         <div class="end-info">
-            <h2>No More pets!</h2>
+            <h2>No More Pets!</h2>
             <p>Come back later to find new pets waiting to be adopted :)</p>
         </div>`;
     $('.pet-card').html(pet_html);
